5ac087749e66d8e0f95c759b85ae83c34c88884a
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-08-12 20:27:52 +0200

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "../components/Navbar.jsx";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+const HeaderFooterLayout = () => {
+  return (
+    <>
+      <MainWrapper>
+        <Navbar />
+        <Outlet />
+      </MainWrapper>
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <HeaderFooterLayout />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "/appartement/:id",
+        element: <CurrentAppartement />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
