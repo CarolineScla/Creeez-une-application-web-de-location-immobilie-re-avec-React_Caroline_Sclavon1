950e13120520ea4a3ed07307d8c4aed73492363a
Caroline Sclavon <caroline.sclavon@gmail.com>
2023-08-18 13:54:29 +0200

    Page Home ok

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,12 @@ import "./footer.scss";
 function Footer() {
   return (
     <div className='footer'>
+          <div className='footer_container'>
        <div className='footer_logo'>
-          <img src='footer_logo.png' alt='logo footer'/>
+          <img src='footerlogo.png' alt='logo footer'></img>
        </div>
        <div className='footer_text'>© 2020 Kasa. All rights reserved
+       </div>
        </div>
     </div>
   )
